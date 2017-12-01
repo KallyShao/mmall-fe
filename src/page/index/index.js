7f34220075975040aa0cdbf47d41488863a048cf
kally <188963758@qq.com>
2017-12-01 17:39:47 +0800

    首页开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,13 +2,40 @@
 * @Author: Administrator
 * @Date:   2017-11-15 11:15:47
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-11-25 18:50:43
+* @Last Modified time: 2017-12-01 17:34:54
 */
 
 'use strict';
-var _mm = require('util/mm.js');
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
+
+var _mm = require('util/mm.js');
 var navSide = require('page/common/nav-side/index.js');
-navSide.init({name: 'order-list'});
+
+var templateIndex = require('./banner.string');
+
+
+
+ 
+
+//初始化slider
+$(function() {
+  //渲染banner的html
+	var html = _mm.renderHtml(templateIndex);
+	$('.banner-con').html(html);
+  //初始化banner
+    var $slider = $('.banner').unslider({
+    	dots: true
+    });
+    // var unslider = $('.banner').unslider();
+
+  $('.banner-con .banner-arrow').click(function() {
+    var forward = $(this).hasClass('prev') ? 'prev' : 'next'; //判断方向，下面的方法是一样的
+    $slider.data('unslider')[forward]();
+     // var fn = this.className.split(' ')[1];
+     // $slider.data('unslider')[fn]();
+  });
+
+});
